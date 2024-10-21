33b3cfed785b34c68adf04ebc9a9cc0f32b09d85
carlosgonzalezplexus <152151109+carlosgonzalezplexus@users.noreply.github.com>
2024-10-21 14:26:09 +0000

    setup ckypress

--- app/src/App.test.cy.tsx
+++ app/src/App.test.cy.tsx
@@ -3,7 +3,6 @@ import App from './App'
 
 describe('<App />', () => {
   it('mounts', () => {
-    // see: https://on.cypress.io/mounting-react
     cy.mount(<App />);
     cy.get('a.App-link').should('have.text', 'Learn React');
     cy.screenshot("main-page");
