8904302844c7f2b9e249da042f56004ba6a7b774
carlosgonzalezplexus <152151109+carlosgonzalezplexus@users.noreply.github.com>
2024-10-23 06:16:44 +0000

    add sass

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,10 +1,11 @@
 import {  useNavigate } from "react-router"
+import './index.scss'
 
 export const MainPage = () => {
   const navigate = useNavigate()
 
   return (<>
-  <h1>Main page</h1>
+  <h1 className="main-title">Main page</h1>
   <button onClick={( ) => {
     navigate("/two")
   }}>Ir a pagina dos</button>
